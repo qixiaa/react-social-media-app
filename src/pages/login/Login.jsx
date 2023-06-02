c7862d7a6d2a31258f32230e21c738024ae72d8c
qixiaa <aqixiaa@gmail.com>
2023-06-03 07:54:17 +0800

    first commit

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import "./login.scss";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus
+            tempora quia nihil vitae delectus? Commodi optio quibusdam vitae sit
+            harum maxime id eos blanditiis quis, reiciendis recusandae doloribus
+            saepe fugit!
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import "./register.scss";
+
+const Register = () => {
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Lama Social.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus
+            tempora quia nihil vitae delectus? Commodi optio quibusdam vitae sit
+            harum maxime id eos blanditiis quis, reiciendis recusandae doloribus
+            saepe fugit!
+          </p>
+          <span>Do you have an account?</span>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
+ 
